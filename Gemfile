ce52d90b69ecbbd8ce32ecd15ed32db32efe82dc
Arun V <arun.vydianathan@gmail.com>
2011-02-04 08:48:10 +0530

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem 'rails', '3.0.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+#gem 'autotest', '4.4.6'
+#gem 'autotest-rails-pure', '4.1.2'
+
+
 
 group :development do
   gem 'rspec-rails', '2.3.0'
@@ -14,6 +18,8 @@ end
 group :test do
   gem 'rspec', '2.3.0'
   gem 'webrat', '0.7.3'
+#  gem 'ZenTest', '4.4.2'
+#  gem 'redgreen', '1.2.2'
 end
 
 # Use unicorn as the web server
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+   end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+      response.should have_selector("title",
+                    :content => @base_title + "Home")
     end
   end
 
@@ -13,6 +20,26 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+      response.should have_selector("title",
+                    :content =>  @base_title + "Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+      response.should have_selector("title",
+                    :content =>  @base_title + "About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+      response.should have_selector("title",
+                    :content =>  @base_title + "Help")
     end
   end
 
